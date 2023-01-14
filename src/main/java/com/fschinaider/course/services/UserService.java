2680055ae401bf1bed7dc270472a5bd9a25b7594
Felipe Schinaider <felipeschinaidernt@gmail.com>
2023-01-14 06:55:33 -0400

    Service layer, component registration

---
+++ src/main/java/com/fschinaider/course/services/UserService.java
@@ -0,0 +1,25 @@
+package com.fschinaider.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.fschinaider.course.entities.User;
+import com.fschinaider.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User>obj = repository.findById(id);
+		return obj.get();
+	}
+}
